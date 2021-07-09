309b5a995614f7d2209c39769c2423f3487fe28f
Germán Pérez <ierman1@hotmail.com>
2021-07-09 11:58:23 +0000

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+        <header>
+            <Navbar></Navbar>
+        </header>
+        <main class="container pt-5">
+            <router-view/>
+        </main>
+        <footer class="bg-secondary text-white text-center p-3">
+            Footer
+        </footer>
+    </div>
+</template>
+
+<style>
+    #app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    main {
+        flex-grow: 1;
+    }
+
+    footer {
+        width: 100%;
+    }
+</style>
+
+<script>
+    import Navbar from '@/components/Navbar.vue'
+
+    export default {
+        name: 'App',
+        components: {
+            Navbar
+        }
+    }
+</script>
+
+<style>
+
+</style>
