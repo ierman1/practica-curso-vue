755950624815b78d165f10b634b4aca8fa698859
w2.gperez <ierman1@hotmail.com>
2021-07-25 12:34:04 +0200

    Seccion info acabada, cambios de accesibilidad generales

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,21 +5,51 @@
 			<table class="table mt-4">
 				<thead>
 					<tr>
-						<th>Nombre</th>
-						<th>Email de contacto</th>
-						<th>¿Busco trabajo?</th>
+						<th scope="col">Nombre</th>
+						<th scope="col">Email de contacto</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
-						<td>Germán Pérez Bordera</td>
+						<td scope="row">Germán Pérez Bordera</td>
 						<td>
 							<a href="mailto:germanperezb98@gmail.com">germanperezb98@gmail.com</a>
 						</td>
-						<td>
-							<span class="sr-only">Sí</span>
-							<font-awesome-icon icon="check"/>
+					</tr>
+				</tbody>
+			</table>
+		</article>
+		<article class="mt-5">
+			<h2 class="font-weight-bold">Tecnologías usadas</h2>
+			<table class="table mt-4">
+				<thead>
+					<tr>
+						<th scope="col">Logo</th>
+						<th scope="col">Nombre</th>
+						<th scope="col">Versión</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td scope="row">
+							<img src="/vue_logo.png" alt="Logo VueJS" height="50">
 						</td>
+						<td>VueJS</td>
+						<td>2.6.14</td>
+					</tr>
+					<tr>
+						<td scope="row">
+							<img src="/bootstrapvue_logo.png" alt="Logo Bootstrap-Vue" height="50">
+						</td>
+						<td>Bootstrap-Vue</td>
+						<td>2.21.2</td>
+					</tr>
+					<tr>
+						<td scope="row">
+							<img src="/fontawesome_logo.png" alt="Logo Fontawesome" height="50">
+						</td>
+						<td>Fontawesome</td>
+						<td>5.15.3</td>
 					</tr>
 				</tbody>
 			</table>
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,43 +1,45 @@
 <template>
 	<section id="tasks">
-		<h2 class="mb-5 font-weight-bold">Gestor de tareas</h2>
-		<form v-on:submit="createTask">
-			<div class="d-flex align-items-center">
-				<b-form-group 
-					label="Nueva tarea"
-					label-for="newTask"
-					class="font-weight-bold flex-grow-1 pr-2"
-					description='Pulsa "/" para acceder directamente'>
-					<b-form-input
-						id="newTask"
-						size="lg"
-						v-model="newTask"
-						type="text"
-						placeholder="Texto de la nueva tarea"
-						required>
-					</b-form-input>
-				</b-form-group>
-				<b-button variant="primary" size="lg" style="margin-top: -8px;" type="submit">
-					<span class="sr-only">
-						Crear nueva tarea
-					</span>
-					<font-awesome-icon icon="plus"></font-awesome-icon>
-				</b-button>
-			</div>
-		</form>
-		<div class="row mb-5">
-			<div class="col-12 col-lg-6">
+		<div role="section">
+			<h2 class="mb-5 font-weight-bold">Gestor de tareas</h2>
+			<form v-on:submit="createTask">
+				<div class="d-flex align-items-center">
+					<b-form-group 
+						label="Nueva tarea"
+						label-for="newTask"
+						class="font-weight-bold flex-grow-1 pr-2"
+						description='Pulsa "/" para acceder directamente'>
+						<b-form-input
+							id="newTask"
+							size="lg"
+							v-model="newTask"
+							type="text"
+							placeholder="Texto de la nueva tarea"
+							required>
+						</b-form-input>
+					</b-form-group>
+					<b-button variant="primary" size="lg" style="margin-top: -8px;" type="submit">
+						<span class="sr-only">
+							Crear nueva tarea
+						</span>
+						<font-awesome-icon icon="plus"></font-awesome-icon>
+					</b-button>
+				</div>
+			</form>
+		</div>
+		<div class="row mb-5" role="section">
+			<article class="col-12 col-lg-6">
 				<div class="todo-tasks mt-5">
 					<h2 class="mb-3">Tareas por hacer</h2>
 					<Task v-for="task in tasks.filter(task => task.status == 'todo')" v-bind:key="task.id" :task="task" @changed="changeStatus" @deleted="deleteTask"></Task>
 				</div>
-			</div>
-			<div class="col-12 col-lg-6">
+			</article>
+			<article class="col-12 col-lg-6">
 				<div class="done-tasks mt-5">
 					<h2 class="mb-3">Tareas completadas</h2>
 					<Task v-for="task in tasks.filter(task => task.status == 'done')" v-bind:key="task.id" :task="task" @changed="changeStatus" @deleted="deleteTask"></Task>
 				</div>
-			</div>
+			</article>
 		</div>
 	</section>
 </template>
